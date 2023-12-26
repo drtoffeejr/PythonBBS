2197681b4e8dfcfd6b0f255608b8e8c0ff87b3c1
drtoffeejr <137294144+drtoffeejr@users.noreply.github.com>
2023-12-26 10:59:47 +0900

    Create bbs.py

---
+++ bbs.py
@@ -0,0 +1,57 @@
+file_path = "/home/ec2-user/environment/10_Submit/bbs.txt"
+
+def new_comment():
+    
+    f = open(file_path, "r", encoding="UTF-8")
+    
+    lines = f.readlines()
+    
+    f.close
+    
+    print("投稿一覧 -------")
+    
+    for line in reversed(lines):
+        print(line.rstrip())
+        
+    print("名前を入力して下さい。")
+    text_NAME = input()
+    
+    print("コメントを入力して下さい。")
+    text_COMM = input()
+    
+    f = open(file_path, "a", encoding="UTF-8")
+    
+    f.write("\n" + text_NAME + "：" + text_COMM)
+    
+    f.close()
+    
+    f = open(file_path, "r", encoding="UTF-8")
+    
+    lines = f.readlines()
+    
+    f.close
+    
+    print("投稿一覧 -------")
+    
+    for line in reversed(lines):
+        print(line.rstrip())
+
+new_comment()
+
+def cont_ques():
+    
+    print("0:終了する 1:書き込む")
+    cont_ans = input()
+    
+    if cont_ans in ["0"]:
+        print("終了します")
+       
+    elif cont_ans in ["1"]:
+        new_comment()
+        cont_ques()
+    
+    else:
+        print("エラー：0か１で入力してください")
+        cont_ques()
+
+cont_ques()
